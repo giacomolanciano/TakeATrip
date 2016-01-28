9237f5e306d42d64d5ebd2e3535b51bed4e045c1
lucagiacomelli <lucagiacomelli1604@gmail.com>
2016-01-28 10:10:20 +0100

    Google Place Android API e Google Route Android API 2

---
+++ mobile/src/main/java/com/example/david/takeatrip/Classes/TwoStepOAuth.java
@@ -0,0 +1,28 @@
+package com.example.david.takeatrip.Classes;
+
+/**
+ * Created by lucagiacomelli on 22/01/16.
+ */
+import org.scribe.builder.api.DefaultApi10a;
+import org.scribe.model.Token;
+
+/**
+ * Generic service provider for two-step OAuth10a.
+ */
+public class TwoStepOAuth extends DefaultApi10a {
+
+    @Override
+    public String getAccessTokenEndpoint() {
+        return null;
+    }
+
+    @Override
+    public String getAuthorizationUrl(Token arg0) {
+        return null;
+    }
+
+    @Override
+    public String getRequestTokenEndpoint() {
+        return null;
+    }
+}
