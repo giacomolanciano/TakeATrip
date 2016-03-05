f916216344ceda5241450d5f0732c5f14f8948d4
lucagiacomelli <lucagiacomelli1604@gmail.com>
2016-03-05 19:15:10 +0100

    Login Google e API facebook per immagini

--- mobile/build/generated/source/r/debug/com/google/android/gms/gcm/R.java
+++ mobile/build/generated/source/r/debug/com/google/android/gms/gcm/R.java
@@ -269,8 +269,8 @@ public final class R {
 		public static final int decline = 0x7f070089;
 		public static final int place_autocomplete_clear_button = 0x7f07003a;
 		public static final int place_autocomplete_search_hint = 0x7f07003b;
-		public static final int store_picture_message = 0x7f070099;
-		public static final int store_picture_title = 0x7f07009a;
+		public static final int store_picture_message = 0x7f07009a;
+		public static final int store_picture_title = 0x7f07009b;
 		public static final int wallet_buy_button_place_holder = 0x7f07003d;
 	}
 	public static final class style {
