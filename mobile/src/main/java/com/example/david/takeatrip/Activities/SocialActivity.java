4f17068c3ddf9a5db7effcb93d61b32deafe6d7b
Davide Mazza <davide181093@gmail.com>
2016-03-12 13:41:51 +0100

    Modifiche Social

--- mobile/src/main/java/com/example/david/takeatrip/Activities/SocialActivity.java
+++ mobile/src/main/java/com/example/david/takeatrip/Activities/SocialActivity.java
@@ -1,12 +1,17 @@
 package com.example.david.takeatrip.Activities;
 
+import android.annotation.TargetApi;
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.ViewGroup;
@@ -15,10 +20,12 @@ import android.widget.ImageView;
 import com.example.david.takeatrip.Classes.Profilo;
 import com.example.david.takeatrip.R;
 import com.example.david.takeatrip.Utilities.DataObject;
+import com.example.david.takeatrip.Utilities.MyRecyclerViewAdapter;
 import com.example.david.takeatrip.Utilities.TabsPagerAdapter;
 
 import java.util.ArrayList;
 
+@TargetApi(Build.VERSION_CODES.LOLLIPOP)
 public class SocialActivity extends FragmentActivity implements ActionBar.TabListener {
 
     private ViewPager viewPager;
@@ -32,12 +39,21 @@ public class SocialActivity extends FragmentActivity implements ActionBar.TabLis
     private ArrayList<DataObject> dataTravels;
     private String email;
 
+    private RecyclerView mRecyclerView;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private static String LOG_TAG = "CardViewActivity";
+
     private ViewGroup group;
     private ImageView image_default;
 
 
     // TODO: Tab titles in other languages
     private String[] tabs = {"Home","Following","Followers", "Top Rated", "Search"};
+
+    private int[] icons = {R.drawable.ic_people_black_36dp,
+           R.drawable.ic_add_a_photo_black_36dp,R.drawable.ic_add_black_24dp,
+            R.drawable.ic_add_white_24dp,
+            R.drawable.ic_cast_light};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,8 +70,8 @@ public class SocialActivity extends FragmentActivity implements ActionBar.TabLis
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
         // Adding Tabs
-        for (String tab_name : tabs) {
-            actionBar.addTab(actionBar.newTab().setText(tab_name)
+        for (int tab_name : icons) {
+            actionBar.addTab(actionBar.newTab().setIcon(tab_name)
                     .setTabListener(this));
         }
 
@@ -90,14 +106,21 @@ public class SocialActivity extends FragmentActivity implements ActionBar.TabLis
         }
 
 
-
-
-
     }
+
+
 
     @Override
     public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
         viewPager.setCurrentItem(tab.getPosition());
+
+
+        if(tab.getPosition()==2){
+            Log.e("TEST", "TAB SELEZIONATO NUMERO 2 ");
+
+        }
+
+        Log.e("TEST", "TAB SELEZIONATO: "+ tab.getPosition() );
 
     }
 
